659530dcc09f1fbecc46158d49e0a8fc5a2d1467
mikeyas alemu <mikeyasalemu933@gmail.com>
2025-07-09 03:57:47 -0700

    data types

---
+++ c++/data types.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    int age;
+    age=14;
+    string name = "Mikeyas"; 
+    cout<<"My name is :"<<name<<endl;   
+    cout<<"My age is :"<<age<<endl;
+return 1;   
+}
+
