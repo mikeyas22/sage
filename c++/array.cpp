33ee57a39e30c508b835975ecaf379d6d0e7b07d
mikeyas alemu <mikeyasalemu933@gmail.com>
2025-07-11 02:56:03 -0700

    array

---
+++ c++/array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+      int main () { 
+      int numbers [10] = {6,9,7,10,3,2,8,17,9,22};
+		for(int i=0;i<9;i++){
+			cout<<numbers[i]<<endl;
+		}      
+	  }
+	  
