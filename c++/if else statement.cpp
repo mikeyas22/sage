e0082fc5e9d1ac19858939adf743ba9e94064e86
mikeyas alemu <mikeyasalemu933@gmail.com>
2025-07-11 02:25:40 -0700

    if else statement

---
+++ c++/if else statement.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+  int main () { 
+      int age ;
+      
+      cout <<"enter your age"<<endl;
+	  cin>>age;
+	  
+	  if(age > 18) {
+	   cout<<"adult" <<endl;
+	   } else{
+	   
+	   cout<<"not adult" <<endl;
+       }
+       return 0; 
+}
+	   
+	  
+	  
