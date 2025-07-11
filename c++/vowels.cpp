932cfb5ff50e6609452dfb0e5ff5839072db0a5b
mikeyas alemu <mikeyasalemu933@gmail.com>
2025-07-11 03:15:32 -0700

    vowels

---
+++ c++/vowels.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+      int main () { 
+      char vowels [5] = {'a','e','i','o','u'};
+	  int i = 0;
+	  
+	  cout << "vowels are: ";
+	  while (i < 5) {
+	  	cout << vowels[i] << " ";
+	  	i++;
+	  }
+	 
+	 cout << endl;
+	 
+	 return 0;
+}
+
+	
+	    
+	   
+	  
