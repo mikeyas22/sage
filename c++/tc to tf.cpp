09c33276c321e90f8ed5c06e7fe306baf99fce52
mikeyas alemu <mikeyasalemu933@gmail.com>
2025-07-11 03:37:45 -0700

    tc to tf

---
+++ c++/tc to tf.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+    int main () {
+    	double tc,tf;
+    	
+    	cout <<"enter tc";
+    	cin >>tc;
+    	
+    	tf = (tc * 9.0/5.0) + 32;
+    	
+    	cout << "t in tf: " << tf << endl;
+    	
+    	return 0;
+	}
